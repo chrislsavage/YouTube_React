f97da559a45f0f27d0f919b549099f5d918ce1c9
Chris Savage <chrislsavage@gmail.com>
2016-02-02 14:36:24 -0500

    add video list and video items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import VideoListItem from './video_list_item'
 const VideoList = (props) => {
+  const videoItems = props.videos.map ((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-    {props.videos.length}
+    {videoItems}
     </ul>
   );
 };
-
+// if you refactor this to a class instead of a functional compenent you will have to change it to this.props...
 export default VideoList;
