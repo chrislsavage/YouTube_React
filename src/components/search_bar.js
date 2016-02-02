93d509ead091d7613cef2d5df1a392aaebe7013f
Chris Savage <chrislsavage@gmail.com>
2016-02-02 12:33:47 -0500

    add video api

---
+++ src/components/search_bar.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+// above is the same as saying const Component = React.Component
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props); //have to call super or you get an error
+    this.state = { term: 'start value' };  //state has a property that we want to record on the state.  term updates the user input.
+  }
+  render() { //this is the default way of defining methods on a class as a function
+    return (
+      <div>
+      <input
+        value = {this.state.term}
+        onChange={event => this.setState({ term: event.target.value})} />
+      </div>
+    );
+  }
+  //A controlled field is a form element that is set by the state.
+  //Whenever you want to manipulate value of input you want to use state.
+  //always use setState to inform react that the state is changing...you must return jsx for it to not throw an error...this refers to the event Handler //On change is a react property
+//one one line you can get rid of the bananas around (event)
+
+  // onInputChange(event) { //or handleInputChange
+  //   console.log(event.target.value);//this is redfined above using a arrow function
+  // }
+}
+// every react class must have a render method
+// const SearchBar = () => {
+//   return <input />;
+// }
+// use a functional component unless you need added functionality.
+
+export default SearchBar;
+
+//declare event handler...and pass event
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+const API_KEY = 'AIzaSyBf7FYyKawCmz2NcWTESBPnjAJYHcQCLK0'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component that will produce HTML
+//always use class when we have  state
+class App extends Component  {
+  constructor(props) {
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: "surfboards"},  (videos) => {
+      this.setState ({videos}); //this.setState ({videos: videos})//if same var name
+    });
+}
+
+  render() {
+    return (
+    <div>
+    <SearchBar />
+    <VideoList videos={this.state.videos} />
+    </div>
+    );
+  }
+}
+//wrap a component in jsx tags to make it an instance
+ReactDOM.render(<App />, document.querySelector('.container'));
